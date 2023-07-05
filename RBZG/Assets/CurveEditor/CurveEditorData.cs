7400bf1f55c9c076f339ac47856fa1e9daa8ec45
JacksonH <jacksonhole24@gmail.com>
2023-07-05 17:50:41 +0930

    UPDATED Curve Editor

---
+++ RBZG/Assets/CurveEditor/CurveEditorData.cs
@@ -0,0 +1,15 @@
+using Codice.Client.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CurveEditor
+{
+    [CreateAssetMenu(fileName = "Curve Data", menuName = "Curve Data")]
+    public class CurveEditorData : ScriptableObject
+    {
+        [SerializeField] public Curve[] curves;
+    }
+}
